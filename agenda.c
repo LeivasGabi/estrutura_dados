1e29b88644f1b93d245343827611e64f14313d85
Leonardo Marcos <marcosleonardosss@gmail.com>
2022-11-01 20:19:34 -0300

    feat: criacao do agenda.c

---
+++ agenda.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include "agenda.h"
+
+int insert (cabecalho *cabeca, contato c) {
+	no *novoNo = (no*)malloc(sizeof(no));
+	
+	if(novoNo == NULL){
+		return 0;
+	}
+  
+	return 1;
+}
